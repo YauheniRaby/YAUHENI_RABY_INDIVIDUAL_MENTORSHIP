c435255afa9c8776a414bf99d0720eb152eb90d9
YauheniRaby <yraby@exadel.com>
2022-02-22 00:30:28 +0300

    fix bag

--- Solution1/Solution1.Tests/BL.Tests/WeatherServiceTests.cs
+++ Solution1/Solution1.Tests/BL.Tests/WeatherServiceTests.cs
@@ -2,11 +2,9 @@
 using BusinessLayer.DTOs;
 using BusinessLayer.Service;
 using BusinessLayer.Service.Abstract;
+using ConsoleApp.AutoMap;
+using KellermanSoftware.CompareNetObjects;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -14,34 +12,37 @@ namespace Solution1.Tests.BL.Tests
 {
     public class WeatherServiceTests
     {
+        private readonly IMapper _mapper;
+        private readonly WeatherService _weatherService;
+        private readonly Mock<IWeatherApiService> _weatherApiServiceMock;
+
+        public WeatherServiceTests()
+        {
+            _weatherApiServiceMock = new Mock<IWeatherApiService>();
+            _mapper = new Mapper(MapperConfig.GetConfiguration());
+            _weatherService = new WeatherService(_mapper, _weatherApiServiceMock.Object);
+        }
+
         [Fact]
-        public async Task GetByCityNameAsync_ReternedWeatherToStringAsync()
+        public async Task GetByCityNameAsync_ReternedWeatherDTO()
         {
             // Arrange
             var cityName = "Minsk";
             var temp = 10;
-            var mockApi = new Mock<IWeatherApiService>();
-            var mockApiResult = new WeatherApiDTO() { CityName = cityName, TemperatureValues = new WeatherApiTempDTO() { Temp = temp } };
-            mockApi.Setup(weatherApi =>
-                        weatherApi
-                         .GetByCityNameAsync(cityName))
-                         .ReturnsAsync(mockApiResult);
+            var WeatherApiDto = new WeatherApiDTO() { CityName = cityName, TemperatureValues = new WeatherApiTempDTO() { Temp = temp } };
+            _weatherApiServiceMock
+                .Setup(weatherApi => 
+                    weatherApi
+                    .GetByCityNameAsync(cityName))
+                    .ReturnsAsync(WeatherApiDto);
 
-            var mockAutoMapper = new Mock<IMapper>();
-            var mockAutoMapperResult = new WeatherDTO() { CityName = cityName, Temp = temp};
-            mockAutoMapper.Setup(weatherApiDTO =>
-                                weatherApiDTO
-                                    .Map<WeatherDTO>(mockApiResult))
-                                    .Returns(mockAutoMapperResult);
+            var expectedObject = new WeatherDTO() { CityName = cityName, Temp = temp, Comment = "It's fresh"};
             
-            var weatherServise = new WeatherService(mockAutoMapper.Object, mockApi.Object);
-
-            var expectedCount = new WeatherDTO() { CityName = cityName, Temp = temp, Comment = "It's fresh"};
             // Act
-            var actualCount = await weatherServise.GetByCityNameAsync(cityName);
+            var actualObject = await _weatherService.GetByCityNameAsync(cityName);
 
             // Assert
-            Assert.Equal(expectedCount.Comment, actualCount.Comment);
+            Assert.True(new CompareLogic().Compare(expectedObject, actualObject).AreEqual);
         }               
     }
 }
