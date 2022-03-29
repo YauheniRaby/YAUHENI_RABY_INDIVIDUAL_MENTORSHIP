38a16dd712b953aa700ade79553204402a62c8f2
YauheniRaby <yraby@exadel.com>
2022-03-30 00:02:40 +0300

    Add invoker

--- Solution1/WeatherApi/Controllers/WeatherController.cs
+++ Solution1/WeatherApi/Controllers/WeatherController.cs
@@ -1,7 +1,10 @@
-﻿using BusinessLayer.DTOs;
+﻿using BusinessLayer.Command;
+using BusinessLayer.Command.Abstract;
+using BusinessLayer.Configuration.Abstract;
+using BusinessLayer.DTOs;
+using BusinessLayer.Infrastructure;
 using BusinessLayer.Services.Abstract;
 using Microsoft.AspNetCore.Mvc;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace WeatherApi.Controllers
@@ -11,23 +14,29 @@ namespace WeatherApi.Controllers
     public class WeatherController : ControllerBase
     {
         private readonly IWeatherServiсe _weatherServiсe;
+        private readonly IConfig _config;
+        private readonly IInvoker _invoker;
 
-        public WeatherController(IWeatherServiсe weatherServiсe)
+        public WeatherController(IWeatherServiсe weatherServiсe, IConfig config, IInvoker invoker)
         {
-            _weatherServiсe = weatherServiсe;            
+            _weatherServiсe = weatherServiсe;
+            _config = config;
+            _invoker = invoker;
         }
 
         [HttpGet("CurrentWeather/{cityName}")]
         public async Task<ActionResult<WeatherDTO>> GetCurrentWeatherByCityName([FromRoute] string cityName)
         {
-            var result = await _weatherServiсe.GetByCityNameAsync(cityName, new CancellationToken(true));
+            var command = new CurrentWeatherCommand(_weatherServiсe, cityName);
+            var result = await _invoker.RunAsync(command, TokenGenerator.GetCancellationToken(_config.RequestTimeout));
             return Ok(result);
         }
         
         [HttpGet("ForecastWeather/{cityName}/{countDays}")]
         public async Task<ActionResult<ForecastWeatherDTO>> GetForecastWeatherByCityName([FromRoute] string cityName, [FromRoute] int countDays)
         {
-            var result = await _weatherServiсe.GetForecastByCityNameAsync(cityName, countDays, CancellationToken.None);
+            var command = new ForecastWeatherCommand(_weatherServiсe, cityName, countDays);
+            var result = await _invoker.RunAsync(command, TokenGenerator.GetCancellationToken(_config.RequestTimeout));
             return Ok(result);
         }
     }
