fd6ae362377850a5cb5c5c0767a0fdf361ea16a3
YauheniRaby <yraby@exadel.com>
2022-04-18 17:21:17 +0300

    test

--- Solution1/BusinessLayer/Infrastructure/DateTimeConverterUsingDateTimeParse.cs
+++ Solution1/BusinessLayer/Infrastructure/DateTimeConverterUsingDateTimeParse.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -8,12 +9,20 @@ namespace BusinessLayer.Infrastructure
     {
         public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
+            CultureInfo provider = CultureInfo.InvariantCulture;
+            string format = "dd/MM/yyyy HH:mm:ss.ffffff";
+
+
             var str = reader.GetString();
             Console.WriteLine($"str - {str}");
+            
             var V1 = DateTime.Parse(reader.GetString());
             Console.WriteLine($"TestParseV1 - {V1.ToString("1 -dd, 2 -MM, 3 -yyyy HH:mm:ss")}");
-            
-            
+
+            var V2 = DateTime.ParseExact(str, format, provider);
+            Console.WriteLine($"TestParseV2 - {V2.ToString("1 -dd, 2 -MM, 3 -yyyy HH:mm:ss")}");
+
+
             ////var V2 = DateTime.ParseExact(reader.GetString(), "yyyy-MM-dd HH:mm:ss", null);
 
             //Console.WriteLine($"TestParseV1 - {V1.ToString("1 -dd, 2 -MM, 3 -yyyy HH:mm:ss")}");
