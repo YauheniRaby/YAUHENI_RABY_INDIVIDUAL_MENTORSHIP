e98d0aeae86cd313109c9a7d3a899b10993dbd21
YauheniRaby <yraby@exadel.com>
2022-04-21 16:33:15 +0300

    All Tests

---
+++ Solution1/Solution1.Tests/BL/Extensions/ForecastWeatherDTOExtensionsTests.cs
@@ -0,0 +1,62 @@
+﻿using BusinessLayer;
+using BusinessLayer.DTOs;
+using BusinessLayer.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Xunit;
+
+namespace Weather.Tests.BL.Extensions
+{
+    public class ForecastWeatherDTOExtensionsTests
+    {
+        private readonly string _cityName = "Minsk";
+        private readonly DateTime _date = new(2022, 10, 10);
+        private readonly double _temperature1 = 18;
+        private readonly string _comment1 = DataAccessLayer.Constants.WeatherComments.Fresh;
+        private readonly double _temperature2 = 25;
+        private readonly string _comment2 = DataAccessLayer.Constants.WeatherComments.GoodWeather;
+
+        [Fact]
+        public void GetMultiStringRepresentation_GetMultiStringRepresentationFromWeatherDTO_Success()
+        {
+            // Arrange
+            var forecastWeatherDTO = GetForecastWeatherDTO(_comment1, _comment2);
+            var culture = new CultureInfo("en-US");
+
+            // Act
+            var result = forecastWeatherDTO.GetMultiStringRepresentation();
+
+            // Assert
+            var expected = $"{_cityName} weather forecast:" +
+                $"{Environment.NewLine}Day 0 ({_date.ToString(BusinessLayer.Constants.DateTimeFormats.Date, culture)}): {_temperature1:f1} C. {_comment1}" +
+                $"{Environment.NewLine}Day 1 ({_date.AddDays(1).ToString(BusinessLayer.Constants.DateTimeFormats.Date, culture)}): {_temperature2:f1} C. {_comment2}";
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void FillCommentByTemp_FillsComment_Success()
+        {
+            // Arrange
+            var forecastWeatherDTO = GetForecastWeatherDTO(null, null);
+
+            // Act
+            forecastWeatherDTO.FillCommentByTemp();
+
+            // Assert            
+            Assert.Equal(_comment1, forecastWeatherDTO.WeatherForPeriod[0].Comment);
+            Assert.Equal(_comment2, forecastWeatherDTO.WeatherForPeriod[1].Comment);
+        }
+
+        private ForecastWeatherDTO GetForecastWeatherDTO(string comment1, string comment2)
+        {
+            var weatherForPeriod = new List<WeatherForDateDTO>()
+            {
+                new WeatherForDateDTO(){ DateTime = _date, Temp = _temperature1, Comment = comment1 },
+                new WeatherForDateDTO(){ DateTime = _date.AddDays(1), Temp = _temperature2, Comment = comment2 }
+            };
+            return new ForecastWeatherDTO() { CityName = _cityName, WeatherForPeriod = weatherForPeriod };
+        }
+    }
+}
---
+++ Solution1/Solution1.Tests/ConsoleApp/Services/UserCommunicateServiceTests.cs
@@ -0,0 +1,301 @@
+﻿using ConsoleApp.Services;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+using FluentValidation;
+using FluentValidation.Results;
+using BusinessLayer.Command.Abstract;
+using BusinessLayer.Command;
+using BusinessLayer.Services.Abstract;
+using BusinessLayer.DTOs;
+using Weather.Tests.Infrastructure;
+using System.Globalization;
+using BusinessLayer.DTOs.Enums;
+using System.Threading;
+using ConsoleApp.Configuration.Abstract;
+using ConsoleApp.Configuration;
+
+namespace Weather.Tests.ConsoleApp.Services
+{
+    class Myclaa
+    {
+        public string CityName { get; set; }
+    };
+
+    public class UserCommunicateServiceTests
+    {
+        private readonly UserCommunicateService _userCommunicationService;
+        private readonly Mock<IWeatherServiсe> _weatherServiceMock;
+        private readonly Mock<ILogger> _loggerMock;
+        private readonly Mock<IInvoker> _invokerMock;
+        private readonly Mock<IConfig> _config;
+        private readonly string _cityName = "Minsk";
+        private readonly double _temp = 5;
+        private readonly string _comment = BusinessLayer.Constants.WeatherComments.Fresh;
+        private readonly DateTime _dateStartForecast = new(2022, 01, 10);
+
+        public static IEnumerable<object[]> ParamsForExceptionHandlingTest =>
+            new List<object[]>
+            {
+                new object[] { "Minsk", $"Unexpected error. Try one time yet.{Environment.NewLine}", null, false },
+                new object[] { default, "Validation error.", null, true },
+                new object[] { "Minsk", $"Request error. Try again later.{Environment.NewLine}", HttpStatusCode.BadRequest, false },
+                new object[] { "AAA", $"Entered incorrect city name. Try one time yet.{Environment.NewLine}", HttpStatusCode.NotFound, false },
+            };
+
+        public UserCommunicateServiceTests()
+        {
+            _loggerMock = new Mock<ILogger>();
+            _invokerMock = new Mock<IInvoker>();
+            _weatherServiceMock = new Mock<IWeatherServiсe>();
+            _config = new Mock<IConfig>();
+            _userCommunicationService = new UserCommunicateService(_loggerMock.Object, _invokerMock.Object, _weatherServiceMock.Object, _config.Object);
+        }
+
+        [Fact]
+        public async Task CommunicateAsync_GetForecastWeather_ShowForecastWeather()
+        {
+            // Arrange
+            var countDays = 3;
+            var consoleOutput = new StringWriter();
+            Console.SetOut(consoleOutput);
+            Console.SetIn(new StringReader($"2{Environment.NewLine}{_cityName}{Environment.NewLine}{countDays}"));
+            var culture = new CultureInfo("en_US");
+
+            var weatherForPeriod = new List<WeatherForDateDTO>();
+            for (int currentCountDays = 0; currentCountDays < countDays; currentCountDays++)
+            {
+                weatherForPeriod.Add(new WeatherForDateDTO() { DateTime = _dateStartForecast.AddDays(currentCountDays), Temp = _temp + currentCountDays, Comment = _comment });
+            }
+            var forecastWeather = new ForecastWeatherDTO() { CityName = _cityName, WeatherForPeriod = weatherForPeriod };
+
+            _invokerMock
+                .Setup(invoker => invoker.RunAsync(It.IsAny<ForecastWeatherCommand>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(forecastWeather);
+
+            SetDefaultValueConfig();
+
+            //Act
+            await _userCommunicationService.CommunicateAsync();
+
+            //Assert            
+            var ferecastRepresentation = $"{_cityName} weather forecast:{Environment.NewLine}";
+            for (int currentCountDays = 0; currentCountDays < countDays; currentCountDays++)
+            {
+                ferecastRepresentation += $"Day {currentCountDays} ({_dateStartForecast.AddDays(currentCountDays).ToString(BusinessLayer.Constants.DateTimeFormats.Date, culture)}): {_temp + currentCountDays:f1} C. {_comment}{Environment.NewLine}";
+            }
+            var expected = $"{Menu.GetMenuRepresentation()}{Environment.NewLine}" +
+                $"Please, enter city name:{Environment.NewLine}" +
+                $"Please, enter count day:{Environment.NewLine}{ferecastRepresentation}";
+
+            _invokerMock.Verify(i => i.RunAsync(It.IsAny<ForecastWeatherCommand>(), It.IsAny<CancellationToken>()));
+            var result  = consoleOutput.ToString();
+            Assert.Equal(expected, consoleOutput.ToString());
+        }
+
+        [Fact]
+        public async Task CommunicateAsync_GetCurrentWeather_ShowCurrentWeather()
+        {
+            // Arrange
+            var consoleOutput = new StringWriter();
+            Console.SetOut(consoleOutput);
+            Console.SetIn(new StringReader($"1{Environment.NewLine}{_cityName}{Environment.NewLine}"));
+
+            var Weather = new WeatherDTO()
+            {
+                CityName = _cityName,
+                Temp = _temp,
+                Comment = _comment
+            };
+            
+            _invokerMock
+                .Setup(invoker => invoker.RunAsync(It.IsAny<CurrentWeatherCommand>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(Weather);
+
+            SetDefaultValueConfig();
+
+            //Act
+            await _userCommunicationService.CommunicateAsync();
+
+            //Assert            
+            var expected = $"{Menu.GetMenuRepresentation()}{Environment.NewLine}" +
+                $"Please, enter city name:{Environment.NewLine}" +
+                $"In {_cityName} {_temp} C. {_comment}{Environment.NewLine}";
+
+            _invokerMock.Verify(i => i.RunAsync(It.IsAny<CurrentWeatherCommand>(), It.IsAny<CancellationToken>()));
+            Assert.Equal(expected, consoleOutput.ToString());
+        }
+
+        [Fact]
+        public async Task CloseApplication_Success()
+        {
+            // Arrange
+            var pattern = $"Сlose the application{Environment.NewLine}$";
+
+            var consoleOutput = new StringWriter();
+            Console.SetOut(consoleOutput);
+            Console.SetIn(new StringReader($"0{Environment.NewLine}"));
+
+            //Act
+            await _userCommunicationService.CommunicateAsync();
+
+            //Assert
+            Assert.Matches(pattern, consoleOutput.ToString());
+        }
+
+        [Theory]
+        [MemberData(nameof(ParamsForExceptionHandlingTest))]
+        public async Task Communicate_EnterCityName_HandlingExceptionAndShowNoticeAsync(string cityName, string message, HttpStatusCode? statusCode, bool IsValidateEror)
+        {
+            // Arrange
+            var exception = statusCode.HasValue ? new HttpRequestException(null, null, statusCode) :
+                            IsValidateEror ? new ValidationException(new List<ValidationFailure>() { new ValidationFailure("CityName", message) }) : new Exception();
+
+            _invokerMock
+                .Setup(invoker =>
+                    invoker
+                    .RunAsync(It.IsAny<CurrentWeatherCommand>(), It.IsAny<CancellationToken>()))
+                .Throws(exception);
+
+            SetDefaultValueConfig();
+
+            var consoleOutput = new StringWriter();
+            Console.SetOut(consoleOutput);
+            Console.SetIn(new StringReader($"1{Environment.NewLine}{cityName}{Environment.NewLine}"));
+
+            //Act
+            await _userCommunicationService.CommunicateAsync();
+
+            //Assert
+            var expected = $"{Menu.GetMenuRepresentation()}{Environment.NewLine}" +
+                $"Please, enter city name:{Environment.NewLine}" +
+                $"{message}";
+
+            if (IsValidateEror)
+            {
+                expected += Environment.NewLine;
+            }
+
+            Assert.Equal(expected, consoleOutput.ToString());
+        }
+
+        [Theory]
+        [InlineData("-1", "Value out of range. Try one time yet.")]
+        [InlineData("a", "Incorrect value. Try one time yet.")]
+        [InlineData(default, "Incorrect value. Try one time yet.")]
+        public async Task CommunicateAsync_EnterIncorrectValueForMenu_ShowNotice(string inputValue, string message)
+        {
+            // Arrange
+            var consoleOutput = new StringWriter();
+            Console.SetOut(consoleOutput);
+            Console.SetIn(new StringReader($"{inputValue}{Environment.NewLine}"));
+
+            //Act
+            await _userCommunicationService.CommunicateAsync();
+
+            //Assert
+            var expected = $"{Menu.GetMenuRepresentation()}{Environment.NewLine}{message}{Environment.NewLine}";
+            Assert.Equal(expected, consoleOutput.ToString());
+        }
+
+        [Theory]
+        //[InlineData(true)]
+        [InlineData(false)]
+        public async Task CommunicateAsync_GetBestWeatherByArrayCityNameAsync_ShowBestWeather(bool isDebugMode)
+        {
+            // Arrange
+            var leadTime = 472;
+
+            var cityName2 = "Moscow";
+            var temp2 = 12;
+            var leadTime2 = 476;
+
+            var cityName3 = "AAA";
+            var leadTime3 = 475;
+            var testError3 = "Test error message";
+
+            var cityName4 = "Paris";
+            var testError4 = "Timeout exceeded";
+
+            var dictionaryWeatherResponsesDTO = new Dictionary<ResponseStatus, IEnumerable<WeatherResponseDTO>>
+            {
+                { ResponseStatus.Successful, new List<WeatherResponseDTO>
+                            {
+                                new WeatherResponseDTO() { CityName = _cityName, ResponseStatus = ResponseStatus.Successful, Temp = _temp, LeadTime = leadTime },
+                                new WeatherResponseDTO() { CityName = cityName2, ResponseStatus = ResponseStatus.Successful, Temp = temp2, LeadTime = leadTime2 }
+                            }
+                },
+                { ResponseStatus.Fail, new List<WeatherResponseDTO>
+                            {
+                                new WeatherResponseDTO() { CityName = cityName3, ResponseStatus = ResponseStatus.Fail, ErrorMessage=testError3, LeadTime = leadTime3 }
+                            }
+                },
+                { ResponseStatus.Canceled, new List<WeatherResponseDTO>
+                            {
+                                new WeatherResponseDTO() { CityName = cityName4, ResponseStatus = ResponseStatus.Canceled, ErrorMessage = testError4 }
+                            }
+                }
+            };
+
+            var consoleOutput = new StringWriter();
+
+            Console.SetOut(consoleOutput);
+            Console.SetIn(new StringReader($"3{Environment.NewLine}{_cityName}, {cityName3}, {cityName2}{Environment.NewLine}"));
+
+            _invokerMock
+                .Setup(invoker => invoker.RunAsync(It.IsAny<BestWeatherCommand>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(dictionaryWeatherResponsesDTO);
+
+            _config
+                .Setup(config => config.WeatherApiConfiguration)
+                .Returns(new WeatherApiConfiguration());
+            _config
+                .Setup(config => config.AppConfiguration)
+                .Returns(new AppConfiguration() { IsDebugMode = isDebugMode});
+
+            //Act
+            await _userCommunicationService.CommunicateAsync();
+
+            //Assert            
+            var bestWeatherRepresentation = $"City with the highest temperature {temp2} C: {cityName2}. " +
+                $"Successful request count: 2, failed: 1, canceled: 1.";
+
+            var successfulResponsesRepresentation = "Success case:" +
+                $"{Environment.NewLine}City: '{_cityName}', Temp: {_temp}, Timer: {leadTime} ms." +
+                $"{Environment.NewLine}City: '{cityName2}', Temp: {temp2}, Timer: {leadTime2} ms.";
+            var failResponsesRepresentation = "On fail:" +
+                $"{Environment.NewLine}City: '{cityName3}', ErrorMessage: {testError3}, Timer: {leadTime3} ms.";
+            var canceledResponsesRepresentation = "On canceled:" +
+                $"{Environment.NewLine}Weather request for '{cityName4}' was canceled due to a timeout.";
+
+            var debugInfoRepresentation = isDebugMode
+                ? $"{successfulResponsesRepresentation}{Environment.NewLine}" +
+                $"{failResponsesRepresentation}{Environment.NewLine}" +
+                $"{canceledResponsesRepresentation}{Environment.NewLine}"
+                : string.Empty;
+
+            var expected = $"{Menu.GetMenuRepresentation()}{Environment.NewLine}" +
+                $"Please, enter array city name (separator symbal - ',') :{Environment.NewLine}" +
+                $"{bestWeatherRepresentation}{Environment.NewLine}{debugInfoRepresentation}";
+
+            _invokerMock.Verify(i => i.RunAsync(It.IsAny<BestWeatherCommand>(), It.IsAny<CancellationToken>()));
+            Assert.Equal(expected, consoleOutput.ToString());
+        }
+
+        private void SetDefaultValueConfig()
+        {
+            _config
+                .Setup(config => config.WeatherApiConfiguration)
+                .Returns(new WeatherApiConfiguration());
+            _config
+                .Setup(config => config.AppConfiguration)
+                .Returns(new AppConfiguration());
+        }
+    }
+}
